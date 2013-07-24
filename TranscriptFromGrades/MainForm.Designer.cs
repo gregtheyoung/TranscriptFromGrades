c236094d1a8919f4b99a142c4f57290ed3491b22
gregtheyoung <github@twinarchtech.com>
2013-07-24 15:14:45 -0400

    Initial commit

---
+++ TranscriptFromGrades/MainForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace TranscriptFromGrades
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.excelFileTextBox = new System.Windows.Forms.TextBox();
+            this.generateTranscriptButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.templateFileTextBox = new System.Windows.Forms.TextBox();
+            this.templateFileButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.openFileButton.Location = new System.Drawing.Point(396, 60);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(30, 23);
+            this.openFileButton.TabIndex = 0;
+            this.openFileButton.Text = "...";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // excelFileTextBox
+            // 
+            this.excelFileTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.excelFileTextBox.Location = new System.Drawing.Point(13, 60);
+            this.excelFileTextBox.Name = "excelFileTextBox";
+            this.excelFileTextBox.Size = new System.Drawing.Size(377, 20);
+            this.excelFileTextBox.TabIndex = 1;
+            // 
+            // generateTranscriptButton
+            // 
+            this.generateTranscriptButton.Location = new System.Drawing.Point(13, 149);
+            this.generateTranscriptButton.Name = "generateTranscriptButton";
+            this.generateTranscriptButton.Size = new System.Drawing.Size(75, 23);
+            this.generateTranscriptButton.TabIndex = 2;
+            this.generateTranscriptButton.Text = "Generate Transcript";
+            this.generateTranscriptButton.UseVisualStyleBackColor = true;
+            this.generateTranscriptButton.Click += new System.EventHandler(this.generateTranscriptButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Grade File:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Transcript Template:";
+            // 
+            // templateFileTextBox
+            // 
+            this.templateFileTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.templateFileTextBox.Location = new System.Drawing.Point(19, 115);
+            this.templateFileTextBox.Name = "templateFileTextBox";
+            this.templateFileTextBox.Size = new System.Drawing.Size(371, 20);
+            this.templateFileTextBox.TabIndex = 5;
+            // 
+            // templateFileButton
+            // 
+            this.templateFileButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.templateFileButton.Location = new System.Drawing.Point(397, 115);
+            this.templateFileButton.Name = "templateFileButton";
+            this.templateFileButton.Size = new System.Drawing.Size(29, 23);
+            this.templateFileButton.TabIndex = 6;
+            this.templateFileButton.Text = "...";
+            this.templateFileButton.UseVisualStyleBackColor = true;
+            this.templateFileButton.Click += new System.EventHandler(this.templateFileButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 198);
+            this.Controls.Add(this.templateFileButton);
+            this.Controls.Add(this.templateFileTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.generateTranscriptButton);
+            this.Controls.Add(this.excelFileTextBox);
+            this.Controls.Add(this.openFileButton);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox excelFileTextBox;
+        private System.Windows.Forms.Button generateTranscriptButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox templateFileTextBox;
+        private System.Windows.Forms.Button templateFileButton;
+    }
+}
+
---
+++ TranscriptFromGrades/MainForm.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Configuration;
+using Excel;
+
+namespace TranscriptFromGrades
+{
+    public partial class MainForm : Form
+    {
+        private string template;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            templateFileTextBox.Text = Properties.Settings.Default.TemplateFile;
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                excelFileTextBox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void templateFileButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                templateFileTextBox.Text = openFileDialog.FileName;
+            }
+
+        }
+
+        private void generateTranscriptButton_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.TemplateFile = templateFileTextBox.Text;
+            Properties.Settings.Default.Save();
+
+            GenerateTranscript(excelFileTextBox.Text, templateFileTextBox.Text);
+        }
+
+        private void GenerateTranscript(string excelFileName, string templateFileName)
+        {
+            DataSet ds = GetDataSetFromExcelFile(excelFileName);
+            template = GetTranscriptTemplate(templateFileName);
+
+            ReplaceStudentInfo(ds);
+
+            ReplaceAcademicRecord(ds);
+
+            CreateTranscript();
+        }
+
+        private void ReplaceAcademicRecord(DataSet ds)
+        {
+            DataTable gradesTable = ds.Tables["Grades"];
+
+            DataView gradesView = gradesTable.AsDataView();
+
+            gradesView.Sort = "SchoolYear ASC, Subject ASC, Title ASC";
+
+            var distinctYears = (from row in gradesTable.AsEnumerable() select row.Field<string>("SchoolYear")).Distinct();
+
+            Dictionary<string, SubjectCredit> subjectCredits = new Dictionary<string, SubjectCredit>();
+            subjectCredits.Add("Bible", new SubjectCredit {matchString="bible", creditsThisYear=0.0, totalCredits=0.0});
+            subjectCredits.Add("English", new SubjectCredit {matchString="english", creditsThisYear=0.0, totalCredits=0.0});
+            subjectCredits.Add("Fine Arts", new SubjectCredit {matchString="finearts", creditsThisYear=0.0, totalCredits=0.0});
+            subjectCredits.Add("Foreign Language", new SubjectCredit {matchString="foreign", creditsThisYear=0.0, totalCredits=0.0});
+            subjectCredits.Add("Mathematics", new SubjectCredit {matchString="math", creditsThisYear=0.0, totalCredits=0.0});
+            subjectCredits.Add("Other", new SubjectCredit {matchString="other", creditsThisYear=0.0, totalCredits=0.0});
+            subjectCredits.Add("Physical Education", new SubjectCredit {matchString="pe", creditsThisYear=0.0, totalCredits=0.0});
+            subjectCredits.Add("Science", new SubjectCredit {matchString="science", creditsThisYear=0.0, totalCredits=0.0});
+            subjectCredits.Add("Service", new SubjectCredit {matchString="service", creditsThisYear=0.0, totalCredits=0.0});
+            subjectCredits.Add("Social Studies", new SubjectCredit {matchString="socialstudies", creditsThisYear=0.0, totalCredits=0.0});
+            subjectCredits.Add("Technology / Trade / Business", new SubjectCredit { matchString = "tech", creditsThisYear = 0.0, totalCredits = 0.0 });
+
+            int sectionNumber = 1;
+            bool first = true;
+            foreach (string year in distinctYears)
+            {
+                if (!String.IsNullOrEmpty(year))
+                {
+                    template = template.Replace("[[year" + sectionNumber + "]]", year);
+
+                    gradesView.RowFilter = "SchoolYear = '" + year + "'";
+
+                    var rows = gradesView.ToTable().AsEnumerable();
+
+                    // Obtain the RTF string that represents one row. We will do this by going from 
+                    // a known tag in row 2 to the same known tag in row 3, and grabbing all
+                    // the text inbetween.
+                    int firstLocation = template.IndexOf("[[year" + sectionNumber + "subject]]");
+                    int secondLocation = template.IndexOf("[[year" + sectionNumber + "subject]]", firstLocation + 1);
+                    string clone = template.Substring(firstLocation, secondLocation - firstLocation);
+
+                    // If we have less than four rows, then we need to remove a row from the RTF template
+                    if (rows.Count() < 4)
+                        template = template.Remove(firstLocation, secondLocation - firstLocation);
+                    else if (rows.Count() > 4)
+                    {
+                        // We need to add rows to the RTF template
+                        for (int i = 0; i < rows.Count() - 4; i++)
+                            template = template.Insert(secondLocation, clone);
+                    }
+
+                    double creditsThisYear = 0.0;
+                    double pointsThisYear = 0.0;
+
+                    foreach (DataRow row in rows)
+                    {
+                        string patternPrefix = "[[year" + sectionNumber;
+                        if (first) patternPrefix += "first";
+                        if (rows.Last() == row) patternPrefix += "last";
+                        template = ReplaceFirstPatternInstanceWithString(template, patternPrefix + "subject]]", row["Subject"].ToString());
+                        template = ReplaceFirstPatternInstanceWithString(template, patternPrefix + "title]]", row["Title"].ToString());
+                        Double credit = new Double();
+                        Double.TryParse(row["Credit"].ToString(), out credit);
+                        template = ReplaceFirstPatternInstanceWithString(template, patternPrefix + "credit]]", credit.ToString("f1"));
+                        template = ReplaceFirstPatternInstanceWithString(template, patternPrefix + "grade]]", row["Final"].ToString());
+                        switch (row["Final"].ToString().ToUpper()[0])
+                        {
+                            case 'A':
+                                pointsThisYear += 4.0 * credit;
+                                break;
+                            case 'B':
+                                pointsThisYear += 3.0 * credit;
+                                break;
+                            case 'C':
+                                pointsThisYear += 2.0 * credit;
+                                break;
+                        }
+
+                        subjectCredits[row["Subject"].ToString()].creditsThisYear += credit;
+                        subjectCredits[row["Subject"].ToString()].totalCredits += credit;
+
+                        creditsThisYear += credit;
+                        first = false;
+                    }
+
+                    template = ReplaceFirstPatternInstanceWithString(template, "[[year" + sectionNumber + "totalcredits]]", creditsThisYear.ToString("f1"));
+                    Double gpaThisYear = pointsThisYear / creditsThisYear;
+                    template = ReplaceFirstPatternInstanceWithString(template, "[[year" + sectionNumber + "gpa]]", gpaThisYear.ToString("f2"));
+
+                    foreach (string key in subjectCredits.Keys)
+                    {
+                        if (subjectCredits[key].creditsThisYear == 0.0)
+                            template = template.Replace("[[year" + sectionNumber + subjectCredits[key].matchString + "]]", "");
+                        else
+                            template = template.Replace("[[year" + sectionNumber + subjectCredits[key].matchString + "]]", subjectCredits[key].creditsThisYear.ToString("f1"));
+                        subjectCredits[key].creditsThisYear = 0.0;
+                    }
+
+                    first = true;
+                    sectionNumber++;
+                }
+
+            }
+            foreach (string key in subjectCredits.Keys)
+            {
+                if (subjectCredits[key].totalCredits == 0.0)
+                    template = template.Replace("[[total" + subjectCredits[key].matchString + "]]", "");
+                else
+                    template = template.Replace("[[total" + subjectCredits[key].matchString + "]]", subjectCredits[key].totalCredits.ToString("f1"));
+            }
+
+            // Get rid of the rest of the tags that haven't been replaced.
+            while (template.IndexOf("[[") >= 0)
+            {
+                int startLocation = template.IndexOf("[[");
+                int endLocation = template.IndexOf("]]");
+                template = template.Remove(startLocation, endLocation - startLocation + 2);
+            }
+        }
+
+        private static string ReplaceFirstPatternInstanceWithString(string originalString, string pattern, string replacement)
+        {
+            string newString = originalString;
+
+            int start = newString.IndexOf(pattern);
+            if (start >= 0)
+            {
+                newString = newString.Remove(start, pattern.Length);
+                newString = newString.Insert(start, replacement);
+            }
+
+            return newString;
+        }
+
+        private void ReplaceStudentInfo(DataSet ds)
+        {
+            DataRow row = ds.Tables["Info"].Select("FirstColumn = 'Name'")[0];
+            SubstituteSingleString(row, @"[[fullname]]");
+
+            row = ds.Tables["Info"].Select("FirstColumn = 'DOB'")[0];
+            SubstituteDate(row, @"[[birthdate]]");
+
+            row = ds.Tables["Info"].Select("FirstColumn = 'Parents'")[0];
+            SubstituteSingleString(row, @"[[parentnames]]");
+
+            row = ds.Tables["Info"].Select("FirstColumn = 'Address1'")[0];
+            SubstituteSingleString(row, @"[[addressline1]]");
+
+            row = ds.Tables["Info"].Select("FirstColumn = 'City'")[0];
+            SubstituteSingleString(row, @"[[city]]");
+
+            row = ds.Tables["Info"].Select("FirstColumn = 'State'")[0];
+            SubstituteSingleString(row, @"[[state]]");
+
+            row = ds.Tables["Info"].Select("FirstColumn = 'Zip'")[0];
+            SubstituteSingleString(row, @"[[zip]]");
+
+            row = ds.Tables["Info"].Select("FirstColumn = 'Phone'")[0];
+            SubstituteSingleString(row, @"[[phone]]");
+
+            row = ds.Tables["Info"].Select("FirstColumn = 'Email'")[0];
+            SubstituteSingleString(row, @"[[email]]");
+
+            row = ds.Tables["Info"].Select("FirstColumn = 'Grad Date'")[0];
+            SubstituteDate(row, @"[[graddate]]");
+            if (row[1].ToString() != "")
+                template = template.Replace("[[diplomaearned]]", "Yes");
+            else
+                template = template.Replace("[[diplomaearned]]", "No");
+
+            row = ds.Tables["Info"].Select("FirstColumn = 'Credits'")[0];
+            SubstituteNumber(row, @"[[totalcredits]]", 1);
+
+            row = ds.Tables["Info"].Select("FirstColumn = 'GPA'")[0];
+            SubstituteNumber(row, @"[[overallgpa]]", 2);
+
+        }
+
+        private void SubstituteSingleString(DataRow row, string pattern)
+        {
+            string newString = row[1].ToString();
+            template = template.Replace(pattern, newString);
+        }
+
+        private void SubstituteDate(DataRow row, string pattern)
+        {
+            string newString = row[1].ToString();
+
+            DateTime dateTime = new DateTime();
+            if (DateTime.TryParse(newString, out dateTime))
+                newString = dateTime.ToShortDateString();
+            else
+                newString = "";
+
+            template = template.Replace(pattern, newString);
+        }
+
+        private void SubstituteNumber(DataRow row, string pattern, int numberOfDigitsAfterDecimalPoint)
+        {
+            string newString = row[1].ToString();
+            
+            Double doubleValue = new Double();
+            if (Double.TryParse(newString, out doubleValue))
+                newString = doubleValue.ToString("f"+numberOfDigitsAfterDecimalPoint);
+            else
+                newString = "";
+
+            template = template.Replace(pattern, newString);
+        }
+
+        private void CreateTranscript()
+        {
+            File.WriteAllText("c:\\Temp\\TranscriptGenerator\\newfile.rtf", template);
+        }
+
+        private static DataSet GetDataSetFromExcelFile(string fileName)
+        {
+            FileStream stream = File.Open(fileName, FileMode.Open, FileAccess.Read);
+
+            IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+
+            excelReader.IsFirstRowAsColumnNames = false;
+            DataSet ds = excelReader.AsDataSet();
+
+            ds.Tables["Info"].Columns[0].ColumnName = "FirstColumn";
+            ds.Tables["Grades"].Columns[0].ColumnName = "SchoolYear";
+            ds.Tables["Grades"].Columns[1].ColumnName = "Subject";
+            ds.Tables["Grades"].Columns[2].ColumnName = "Title";
+            ds.Tables["Grades"].Columns[7].ColumnName = "Final";
+            ds.Tables["Grades"].Columns[8].ColumnName = "Credit";
+            ds.Tables["Grades"].Rows[0].Delete();
+            ds.AcceptChanges();
+
+            stream.Close();
+            excelReader.Close();
+
+            return ds;
+        }
+
+        private static string GetTranscriptTemplate(string fileName)
+        {
+            StreamReader reader = File.OpenText(fileName);
+            string allText = reader.ReadToEnd();
+
+            reader.Close();
+
+            return allText;
+        }
+
+    }
+
+    class SubjectCredit
+    {
+        public string matchString;
+        public double creditsThisYear;
+        public double totalCredits;
+    }
+}
